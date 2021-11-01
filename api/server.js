d041b3f91ede895d712e48284f140c6e75118c6a
CamMeiners <cammeiners@gmail.com>
2021-11-01 17:13:24 -0600

    almost done, minor debugging time

--- api/server.js
+++ api/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 
-const router = require('./accounts/accounts-router');
+const accountsRouter = require('./accounts/accounts-router');
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/acounts', router);
+
+server.use('/api/accounts', accountsRouter);
+
 server.use('*', (req, res) => {
   res.status(404).json({
     message: 'it didnt work man!',
   });
 });
+
 module.exports = server;
